ade62056e88c6d267fdecf20b731910ff4a3df87
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2023-10-09 20:40:21 +0800

    Day 1
    Membuat project, dan menampilkan hello world

---
+++ Pertemuanpertama.java
@@ -0,0 +1,10 @@
+package kia;
+
+public class zas {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        //ini adalah codingan pertama
+        //pertemuan perdana
+        //*100 days coding
